67e86ceb16c878612cbb5944710e409acd90f038
Tejas Shah <tejasanilshah@gmail.com>
2017-09-16 11:38:53 +0530

    Dicee App
    This app simulates the roll of two dies like at a Las Vegas Casino

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Tejas Anil Shah on 14/09/17.
+//  Copyright © 2017 Tejas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed() {
+        let randomNumber1 = arc4random_uniform(6) + 1
+        let randomNumber2 = arc4random_uniform(6) + 1
+        diceImage1.image = UIImage(named: "dice" + "\(randomNumber1)" )
+        diceImage2.image = UIImage(named: "dice" + "\(randomNumber2)" )
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            rollButtonPressed()
+        }
+    }
+
+}
+
