0cd98a20116c11fdaf47bd0fa78f4fdc4d3c4b9b
Tejas Shah <tejasanilshah@gmail.com>
2017-09-18 23:58:45 +0530

    Dicee Code version

---
+++ Dicee Code/Dicee Code/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Dicee Code
+//
+//  Created by Tejas Anil Shah on 16/09/17.
+//  Copyright © 2017 Tejas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func setUpUI() -> Void {
+        <#function body#>
+    }
+
+}
+
